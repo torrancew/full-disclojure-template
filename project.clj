668a908ab423fdb09e8a371df8f19b8b085c0a2f
Tray Torrance <torrancew@gmail.com>
2016-01-27 08:14:57 -0800

    Version bump: v0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject full-disclojure/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject full-disclojure/lein-template "0.1.0"
+  :description "A Leiningen template for webapps built with Compojure and Om"
+  :url "https://github.com/torrancew/full-disclojure-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
